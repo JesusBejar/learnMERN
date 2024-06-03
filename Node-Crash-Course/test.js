1df2a687ebe66998754e6dbda6a76a56214763ea
Jesus Bejar <israelbejarii@gmail.com>
2024-06-03 15:16:07 -0600

    made simple function w template string

--- Node-Crash-Course/test.js
+++ Node-Crash-Course/test.js
@@ -1,3 +1,10 @@
 const namename = "mario";
 console.log(namename)
 
+//simple function w template string
+const salute = (namename) => {
+    console.log(`hola, ${namename}`)
+}
+
+salute ('gorbochav');
+salute ('drago')
