c7650d2bff7b43c6d7d8ea67474dfb4f7472e9af
Jesus Bejar <israelbejarii@gmail.com>
2024-06-08 18:58:26 -0600

    learned how to write simple files

--- Node-Crash-Course/v2/files.js
+++ Node-Crash-Course/v2/files.js
@@ -2,15 +2,32 @@
 const fs = require('fs');
 
 // read files
-fs.readFile("./docs/file.txt", (err, data) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log(data);
-})
+// parameters = path, callback function
+// REMEMBER TO UNCOMMENT----------------------------------------------------------------------------------
+// fs.readFile("./docs/file.txt", (err, data) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     // below logs buffer (a package of data)
+//     console.log(data);
+//     // add .to String function to translate buffer
+//     console.log(data.toString());
+
+// })
 
 // write files
+// parameters = path, message, callback function
+// REMEMBER TO UNCOMMENT----------------------------------------------------------------------------------
+
+fs.writeFile("./docs/file.txt", "hola mundo", () => {
+    console.log("file was written");
+})
+// if file path doesn't exist, file is created
+fs.writeFile("./docs/file2.txt", "hola mundo otra vez", () => {
+    console.log("file was written");
+})
 
 // delete files
 
 // directories
+
