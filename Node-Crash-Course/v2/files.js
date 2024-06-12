80db133a329e1f25b3fe64bc12921abd1b5a8bc4
Jesus Bejar <israelbejarii@gmail.com>
2024-06-11 20:13:08 -0600

    wrote some simple delete file code

--- Node-Crash-Course/v2/files.js
+++ Node-Crash-Course/v2/files.js
@@ -1,5 +1,6 @@
 // file system - core module
 const fs = require('fs');
+// BEST FOR SMALLER FILES
 
 // read files
 // parameters = path, callback function
@@ -19,15 +20,39 @@ const fs = require('fs');
 // parameters = path, message, callback function
 // REMEMBER TO UNCOMMENT----------------------------------------------------------------------------------
 
-fs.writeFile("./docs/file.txt", "hola mundo", () => {
-    console.log("file was written");
-})
-// if file path doesn't exist, file is created
-fs.writeFile("./docs/file2.txt", "hola mundo otra vez", () => {
-    console.log("file was written");
-})
+// fs.writeFile("./docs/file.txt", "hola mundo", () => {
+//     console.log("file was written");
+// })
+// // if file path doesn't exist, file is created
+// fs.writeFile("./docs/file2.txt", "hola mundo otra vez", () => {
+//     console.log("file was written");
+// })
 
 // delete files
-
+if(fs.existsSync("./docs/delete.txt")) {
+    fs.unlink("./docs/delete.txt", (err) => {
+        if (err) {
+            console.log(err)
+        }
+        console.log("file deleted");
+    })
+}
 // directories
-
+// .mkdir is a function that makes directories
+// parameters = path, callback functions
+// if (!fs.existsSync("./assets")) {
+//     fs.mkdir("./assets", (err) => {
+//         if (err) {
+//             console.log(err);
+//         }
+//         console.log("folder created");
+//     })
+// } else {
+//     // .rmdir is a fcunction that deletes directories
+//     fs.rmdir("./assets", (err) => {
+//         if (err) {
+//             console.log(err)
+//         }
+//         console.log("folder deleted");
+//     })
+// }
