97b30f2e9aa7f0f68f0f95b05d6803ad9c0d2e2c
Jesus Bejar <israelbejarii@gmail.com>
2024-06-12 10:26:23 -0600

    made server and wrote request object code

--- Node-Crash-Course/v3/server.js
+++ Node-Crash-Course/v3/server.js
@@ -1,2 +1,14 @@
 const http = require("http");
+// req, res are both objects
+// request object practice
+const server = http.createServer((req, res) => {
+    // console.log("request made");
+    // console.log(req);
+    console.log(req.url, req.method);
+});
 
+// parameters = port number, domain name, callback function
+// localhost is a loop back domain
+server.listen(3000, "localhost", () => {
+    console.log("listening on port 3000")
+});
