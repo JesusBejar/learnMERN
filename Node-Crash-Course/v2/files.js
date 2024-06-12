bafcdd5bedd09fd55e2c23ddaa6c4abdcb49ec7b
Jesus Bejar <israelbejarii@gmail.com>
2024-06-11 20:14:16 -0600

    uncommented file functions

--- Node-Crash-Course/v2/files.js
+++ Node-Crash-Course/v2/files.js
@@ -4,31 +4,29 @@ const fs = require('fs');
 
 // read files
 // parameters = path, callback function
-// REMEMBER TO UNCOMMENT----------------------------------------------------------------------------------
-// fs.readFile("./docs/file.txt", (err, data) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     // below logs buffer (a package of data)
-//     console.log(data);
-//     // add .to String function to translate buffer
-//     console.log(data.toString());
+fs.readFile("./docs/file.txt", (err, data) => {
+    if (err) {
+        console.log(err);
+    }
+    // below logs buffer (a package of data)
+    console.log(data);
+    // add .to String function to translate buffer
+    console.log(data.toString());
 
-// })
+})
 
 // write files
 // parameters = path, message, callback function
-// REMEMBER TO UNCOMMENT----------------------------------------------------------------------------------
-
-// fs.writeFile("./docs/file.txt", "hola mundo", () => {
-//     console.log("file was written");
-// })
-// // if file path doesn't exist, file is created
-// fs.writeFile("./docs/file2.txt", "hola mundo otra vez", () => {
-//     console.log("file was written");
-// })
+fs.writeFile("./docs/file.txt", "hola mundo", () => {
+    console.log("file was written");
+})
+// if file path doesn't exist, file is created
+fs.writeFile("./docs/file2.txt", "hola mundo otra vez", () => {
+    console.log("file was written");
+})
 
 // delete files
+// parameters = path, callback functions
 if(fs.existsSync("./docs/delete.txt")) {
     fs.unlink("./docs/delete.txt", (err) => {
         if (err) {
@@ -40,19 +38,19 @@ if(fs.existsSync("./docs/delete.txt")) {
 // directories
 // .mkdir is a function that makes directories
 // parameters = path, callback functions
-// if (!fs.existsSync("./assets")) {
-//     fs.mkdir("./assets", (err) => {
-//         if (err) {
-//             console.log(err);
-//         }
-//         console.log("folder created");
-//     })
-// } else {
-//     // .rmdir is a fcunction that deletes directories
-//     fs.rmdir("./assets", (err) => {
-//         if (err) {
-//             console.log(err)
-//         }
-//         console.log("folder deleted");
-//     })
-// }
+if (!fs.existsSync("./assets")) {
+    fs.mkdir("./assets", (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log("folder created");
+    })
+} else {
+    // .rmdir is a fcunction that deletes directories
+    fs.rmdir("./assets", (err) => {
+        if (err) {
+            console.log(err)
+        }
+        console.log("folder deleted");
+    })
+}
