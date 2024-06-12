0a98a07e33727c0ea729d6a9ae4b2b8a680a2c0b
Jesus Bejar <israelbejarii@gmail.com>
2024-06-12 10:32:12 -0600

    wrote respond object code

--- Node-Crash-Course/v3/server.js
+++ Node-Crash-Course/v3/server.js
@@ -5,6 +5,16 @@ const server = http.createServer((req, res) => {
     // console.log("request made");
     // console.log(req);
     console.log(req.url, req.method);
+
+    // respond object practice
+    // set header content type
+    // res.setHeader("Content-Type", "text/plain");
+    res.setHeader("Content-Type", "text/html");
+
+    // res.write("hola broski");
+    res.write("<p>hola broski!<p>");
+
+    res.end();
 });
 
 // parameters = port number, domain name, callback function
